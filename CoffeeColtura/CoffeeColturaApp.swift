5ce1537e072276716b0f0869c0e6eaa00f91066c
Tyler Boyd <tboyd.evan@gmail.com>
2021-06-20 20:12:13 -0500

    Initial Commit

---
+++ CoffeeColtura/CoffeeColturaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeColturaApp.swift
+//  CoffeeColtura
+//
+//  Created by Tyler Boyd on 6/20/21.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeColturaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
